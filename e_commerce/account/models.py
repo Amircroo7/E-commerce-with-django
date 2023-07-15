949d58e0d2a8cc8557480b1c3fb28f8e1a9c1e8d
Amircroo7 <a.a.amirhasan1921@gmail.com>
2023-07-15 11:43:30 +0330

    Creating UserManager

--- e_commerce/account/models.py
+++ e_commerce/account/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
-# Create your models here.
-
+from .managers import UserManager
 
 class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
     phone_number = models.CharField(max_length=11, unique=True)
-    fullname = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'phone_number'
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = ['email','full_name']
 
     def __str__(self):
         return self.email
