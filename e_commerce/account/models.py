87cb48cc0feef2ecf1bc7a03d7d2ca8a8890206c
Amircroo7 <a.a.amirhasan1921@gmail.com>
2023-07-15 11:26:29 +0330

    Creating custom user model

--- e_commerce/account/models.py
+++ e_commerce/account/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 # Create your models here.
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    fullname = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, objects=None):
+        return True 
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'account'
+    'account',
 ]
 
 MIDDLEWARE = [
