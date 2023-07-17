896c2df29e5db7ec5ddf2ad0b181177d8ca20972
Amircroo7 <a.a.amirhasan1921@gmail.com>
2023-07-17 15:05:38 +0330

    Debuging for filtering category

--- e_commerce/home/views.py
+++ e_commerce/home/views.py
@@ -3,14 +3,15 @@ from django.views import View
 from .models import Product, Category
 # Create your views here.
 class HomeView(View):
-    def get(self, request, category_slug=None):
-        products = Product.objects.filter(available=True)
-        categories = Category.objects.filter(is_subcategory=False)
-        if category_slug:
-            category = Category.objects.get(slug=category_slug)
-            products = products.filter(category=category)
-        return render(request, 'home/home.html', {'products': products}, {'categories': categories})
-    
+	def get(self, request, category_slug=None):
+		products = Product.objects.filter(available=True)
+		categories = Category.objects.filter(is_sub=False)
+		if category_slug:
+			category = Category.objects.get(slug=category_slug)
+			products = products.filter(category=category)
+		return render(request, 'home/home.html', {'products':products, 'categories':categories})
+
+
 class ProductDetailView(View):
 	def get(self, request, slug):
 		product = get_object_or_404(Product, slug=slug)
